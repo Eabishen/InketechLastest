f94c773be47440b25f5451e9947004803a6a1e8b
Eabishen <ebi4lbe@gmail.com>
2023-03-16 19:32:07 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// Initialize Swiper main
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
+
+var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+function getHeight() {
+    const heightele = document.querySelector(".bottom");
+    let wrapperheight = document.querySelector(".wholewrap");
+
+
+    let dynamicHeighter = heightele.offsetHeight
+
+    wrapperheight.style.height = `${dynamicHeighter}px ${- 200}px`
+}
+
+const swiperbtn = document.querySelector(".swip")
+
+swiperbtn.addEventListener("click", function () {
+    getHeight();
+})
+
+
+getHeight();
